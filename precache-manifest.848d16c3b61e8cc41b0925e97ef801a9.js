f93e6083d6e6c3b6edd56b53b744e5a7fca1331e
gggTalbiggg <ga_talbi@esi.dz>
2020-06-29 12:26:03 +0100

    Updates

---
+++ precache-manifest.848d16c3b61e8cc41b0925e97ef801a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59324b95fe7eacc94d1d97a977e66e96",
+    "url": "/RobotContact/index.html"
+  },
+  {
+    "revision": "ac3a20c35612e0d5c371",
+    "url": "/RobotContact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e65fb7923b4cd11ea0f0",
+    "url": "/RobotContact/static/css/main.0cf388e8.chunk.css"
+  },
+  {
+    "revision": "ac3a20c35612e0d5c371",
+    "url": "/RobotContact/static/js/2.f5016c65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotContact/static/js/2.f5016c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65fb7923b4cd11ea0f0",
+    "url": "/RobotContact/static/js/main.a478b591.chunk.js"
+  },
+  {
+    "revision": "f3be27300a8dd804837f",
+    "url": "/RobotContact/static/js/runtime-main.ab58f8bc.js"
+  }
+]);
